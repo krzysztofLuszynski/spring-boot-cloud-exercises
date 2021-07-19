3cee68001f3a84fbb6038744c58da0cfe9f53081
Łuszyński <e-kfuk@gft.com>
2021-07-19 11:10:20 +0200

    Added first test for REST controller.

---
+++ simple-service/src/test/java/kluszynski/example/simpleservice/controller/SimpleRESTControllerTest.java
@@ -0,0 +1,29 @@
+package kluszynski.example.simpleservice.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+import java.net.URL;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SimpleRESTControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void getHello() throws Exception {
+
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                new URL("http://localhost:" + port + "/simple-service/noParametersEndpoint").toString(), String.class);
+        assertThat(response.getBody()).isEqualTo("OK");
+    }
+}
