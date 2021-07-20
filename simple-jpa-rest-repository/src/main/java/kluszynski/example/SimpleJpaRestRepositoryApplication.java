414b78f7e7a8ce0e92881c1325692a3c7ca5f448
Łuszyński <e-kfuk@gft.com>
2021-07-20 16:39:27 +0200

    Removed not needed code.

--- simple-jpa-rest-repository/src/main/java/kluszynski/example/SimpleJpaRestRepositoryApplication.java
+++ simple-jpa-rest-repository/src/main/java/kluszynski/example/SimpleJpaRestRepositoryApplication.java
@@ -1,11 +1,7 @@
 package kluszynski.example;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @SpringBootApplication
 public class SimpleJpaRestRepositoryApplication {
@@ -13,15 +9,4 @@ public class SimpleJpaRestRepositoryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleJpaRestRepositoryApplication.class, args);
 	}
-
-	/*
-	@Bean
-	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-		final ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-		final MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter(mapper);
-		return converter;
-	}
-
-	 */
 }
