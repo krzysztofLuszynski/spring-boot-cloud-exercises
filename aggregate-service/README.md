2b986f2f9510efa9e664a3cc3dfe11c4b91f3deb
Łuszyński <e-kfuk@gft.com>
2021-07-26 10:33:51 +0200

    Added aggregate-service with actuator only.

---
+++ aggregate-service/README.md
@@ -0,0 +1,9 @@
+# aggregate-service
+Service calling phones-and-persons.
+
+## Usage
+
+### Actuator
+```
+curl -v http://localhost:8083/aggregate-service/actuator
+```
---
+++ aggregate-service/src/main/java/kluszynski/example/AggregateServiceApplication.java
@@ -0,0 +1,12 @@
+package kluszynski.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggregateServiceApplication.class, args);
+	}
+}
