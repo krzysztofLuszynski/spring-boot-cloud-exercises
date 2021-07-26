e3ec67060ce17a3fc48dda35fe3eb1f1793a64a4
Łuszyński <e-kfuk@gft.com>
2021-07-26 09:54:46 +0200

    Added initial data in data.sql.

---
+++ simple-jpa-rest-repository/src/main/resources/data.sql
@@ -0,0 +1,25 @@
+-------------------------------------------------------------------------------
+-- Table person
+INSERT INTO person(id, first_name, last_name, birth_date, height_in_centimeters)
+VALUES (1, 'Jack', 'White', {ts '2010-01-01 20:00:02.00'}, 186);
+
+INSERT INTO person(id, first_name, last_name, birth_date, height_in_centimeters)
+VALUES (2, 'John', 'Fruciante', {ts '2000-01-01 20:00:02.00'}, 190);
+
+INSERT INTO person(id, first_name, last_name, birth_date, height_in_centimeters)
+VALUES (3, 'Jimmy', 'Page', {ts '1950-01-01T20:00:02'}, 179);
+
+INSERT INTO person(id, first_name, last_name, birth_date, height_in_centimeters)
+VALUES (4, 'John', 'Bohnam', {ts '1952-01-01T20:00:02'}, 169);
+
+INSERT INTO person(id, first_name, last_name, birth_date, height_in_centimeters)
+VALUES (5, 'Robby', 'Williams', {ts '1980-01-01T20:00:02'}, 185);
+
+INSERT INTO person(id, first_name, last_name, birth_date, height_in_centimeters)
+VALUES (6, 'John', 'Zorn', {ts '1955-01-01T20:00:02'}, 183);
+
+INSERT INTO person(id, first_name, last_name, birth_date, height_in_centimeters)
+VALUES (7, 'Anna', 'Van Lare', {ts '1978-01-01T20:00:02'}, 187);
+
+INSERT INTO person(id, first_name, last_name, birth_date, height_in_centimeters)
+VALUES (8, 'Edith', 'Piath', {ts '1930-01-01T20:00:02'}, 150);
