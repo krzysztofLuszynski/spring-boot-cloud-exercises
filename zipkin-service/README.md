95804bea618a5f2a758d06bea6eec65bd21783ad
Łuszyński <e-kfuk@gft.com>
2021-07-29 12:12:40 +0200

    Fired zipkin as spring boot application.

--- zipkin-service/README.md
+++ zipkin-service/README.md
@@ -1,9 +1,16 @@
 # zipkin-service
 Zipkin service.
 
+
 ## Usage
+
+Run from the upper folder.
+
+```
+gradle :zipkin-service:bootRun
+```
 
 ```
 # eureka service
-curl -v http://localhost:8761
+curl -v http://localhost:9411/zipkin
 ```
--- zipkin-service/src/main/java/kluszynski/example/ZipkinServiceApplication.java
+++ zipkin-service/src/main/java/kluszynski/example/ZipkinServiceApplication.java
@@ -2,8 +2,12 @@ package kluszynski.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
+//@EnableEurekaClient
+@EnableZipkinServer
 public class ZipkinServiceApplication {
 
 	public static void main(String[] args) {
