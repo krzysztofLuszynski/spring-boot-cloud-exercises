b63e8189166b394a81a60c7c64ed13194752994a
Łuszyński <e-kfuk@gft.com>
2021-07-29 12:20:31 +0200

    Can not run zipkin and register it in the eureka server.

--- zipkin-service/src/main/java/kluszynski/example/ZipkinServiceApplication.java
+++ zipkin-service/src/main/java/kluszynski/example/ZipkinServiceApplication.java
@@ -2,11 +2,9 @@ package kluszynski.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
-//@EnableEurekaClient
 @EnableZipkinServer
 public class ZipkinServiceApplication {
 
